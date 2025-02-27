4f828e8511d7cdffd4d0ad36fa1c1fbb043e763f
Sergio Di Fiore <difiores@outlook.com>
2025-02-27 16:50:01 -0300

    Main test

--- BioGPTcc/BioGPTcc.py
+++ BioGPTcc/BioGPTcc.py
@@ -1,1 +1,11 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+tokenizer = AutoTokenizer.from_pretrained("microsoft/biogpt")
+model = AutoModelForCausalLM.from_pretrained("microsoft/biogpt").to("cuda")
+
+input_text = "Covid-19 is"
+inputs = tokenizer(input_text, return_tensors="pt").to("cuda")
+outputs = model.generate(**inputs)
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print(generated_text)
